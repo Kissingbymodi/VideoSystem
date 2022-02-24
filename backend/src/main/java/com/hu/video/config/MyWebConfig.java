c6f11c2916efb2dfd9844395727fa7c8e76093ad
Legend丶Hu <42301766+Kissingbymodi@users.noreply.github.com>
2022-02-24 22:24:13 +0800

    Update MyWebConfig.java

--- backend/src/main/java/com/hu/video/config/MyWebConfig.java
+++ backend/src/main/java/com/hu/video/config/MyWebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        # 修改为自己的电脑绝对路径
         registry.addResourceHandler("/static/video/**").addResourceLocations("file:F:\\code\\design\\postDesign\\src\\main\\resources\\static\\video\\");
         registry.addResourceHandler("/static/uimages/**").addResourceLocations("file:F:\\code\\design\\postDesign\\src\\main\\resources\\static\\uimages\\");
     }
